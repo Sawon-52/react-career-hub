ce606c1694b3e8e92043af58619c2a294a9922cf
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-16 19:51:24 +0600

    using Footer from Daisy UI and configure react router

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+// import Home from "../Home/Home";
+
+const Root = () => {
+  return (
+    <div>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
