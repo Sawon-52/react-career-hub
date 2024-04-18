e38d447223a03f7d37858ad77a76d8e91b461d77
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-18 06:56:15 +0600

    sort applied job using Three categories All, Reomte and Onsite

--- src/Components/Applied/Applied.jsx
+++ src/Components/Applied/Applied.jsx
@@ -6,6 +6,7 @@ import AppliedJob from "../AppliedJob/AppliedJob";
 const Applied = () => {
   const jobs = useLoaderData();
   const [appliedJob, setAppliedJob] = useState([]);
+  const [displyJob, setDisplayJob] = useState([]);
 
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
@@ -19,15 +20,44 @@ const Applied = () => {
         }
       }
       setAppliedJob(jobsApplied);
+      setDisplayJob(jobsApplied);
       // console.log(jobs, storedJobIds, jobsApplied);
     }
   }, [jobs]);
 
+  const handleJobFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJob(appliedJob);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJob.filter((job) => job.remote_or_onsite === "Remote");
+      setDisplayJob(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJob.filter((job) => job.remote_or_onsite === "Onsite");
+      setDisplayJob(onsiteJobs);
+    }
+  };
+
   return (
     <div className="w-4/5 mx-auto">
       <h2 className="text-center text-3xl font-bold my-10"> Applied Jobs</h2>
+      <div className="flex justify-end ">
+        <details className="dropdown">
+          <summary className="m-1 btn">Sort Job</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 ">
+            <li onClick={() => handleJobFilter("all")}>
+              <a>All</a>
+            </li>
+            <li onClick={() => handleJobFilter("remote")}>
+              <a>Remote</a>
+            </li>
+            <li onClick={() => handleJobFilter("onsite")}>
+              <a>Onsite</a>
+            </li>
+          </ul>
+        </details>
+      </div>
       <div className="my-4">
-        {appliedJob.map((job) => (
+        {displyJob.map((job) => (
           <AppliedJob job={job} key={job.id}></AppliedJob>
         ))}
       </div>
